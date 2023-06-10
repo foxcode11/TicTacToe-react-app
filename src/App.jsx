b008699d9766eb36f7ae5798d02c23d95e265a51
Deepak Patel <foxcode11@gmail.com>
2023-06-10 16:12:32 +0530

    Winner is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import './style.scss';
 import Board from './components/Board';
+import { useState } from 'react';
+import { calculateWinner } from './winner';
 function App() {
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(square);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `NextPlayer is ${nextPlayer}`;
+  const handleSquareClick = ClickedPosition => {
+    if (square[ClickedPosition] || winner) {
+      return;
+    }
+
+    setSquare(curSquare => {
+      return curSquare.map((squareValue, position) => {
+        if (ClickedPosition == position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+
+    setIsXNext(curIsXNext => !curIsXNext);
+  };
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board square={square} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
